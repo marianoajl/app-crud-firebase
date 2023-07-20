05a7ef4d74a57d23bb79aed83b63f733b7e1ae33
marianoajl <marianoajl@hotmail.com>
2023-07-20 04:16:21 -0300

    Instalación de gh-pages

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig/firebase.js";
 
